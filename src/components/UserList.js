5de66450f515abb62bda53337bdee794e6be0bdc
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 20:46:58 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -13,11 +12,10 @@ const UserList = () => {
   return (
     <div>
       <h1>User List</h1>
-
       <ul>
         {users.map(user => (
           <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
+            <a href={`/users/${user.id}`}>{user.name}</a>
           </li>
         ))}
       </ul>
