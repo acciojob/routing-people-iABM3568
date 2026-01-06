0cb1f5d4a885f32fdc4f4d394f0889897fc01220
Abhishek Mishra <iabm3568@gmail.com>
2026-01-06 20:54:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={UserList} />
-        <Route path="/users/:id" component={UserDetails} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+      </Routes>
     </BrowserRouter>
   );
 };
